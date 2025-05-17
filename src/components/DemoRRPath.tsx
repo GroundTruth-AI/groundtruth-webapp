3e36f6243e3ded7d54c17ac76371a9606f7fcdc0
Brian Robocop Sokas <brian@groundtruthai.org>
2025-05-17 12:06:32 -0400

    Better signal and card setup for the demo path items

--- src/components/DemoRRPath.tsx
+++ src/components/DemoRRPath.tsx
@@ -1,26 +1,18 @@
-import { ArrowRight, ArrowRightAlt, ArrowRightAltOutlined, DoubleArrow } from "@suid/icons-material";
-import { Avatar, Card, CardContent, CardHeader, CircularProgress, Divider, Stack, Typography } from "@suid/material";
-import { createRenderEffect, createSignal, JSX, Match, Show, Switch } from "solid-js"
-import logo from '../assets/lightbulb_logo.png'
+import { ArrowRightAlt } from "@suid/icons-material";
+import { Box, Card, CardContent, CircularProgress, Grow, Skeleton, Stack, Typography } from "@suid/material";
+import { Accessor, createMemo, createSignal, JSX, onMount, Show } from "solid-js"
 
 const sleep = (ms: number) => new Promise(res => setTimeout(res, ms));
 
-const PathItem = ({ loadingText, children, showContent, id }: { children?: JSX.Element, loadingText: string, showContent: boolean, id: string }) => {
+const PathItem = ({ loadingText, children, showContent, id }: { children?: JSX.Element, loadingText: string, showContent: Accessor<boolean>, id: string }) => {
     return (
-        <Show when={showContent} fallback={
-            <Stack alignItems="center" gap={'1rem'}>
-                <Typography variant="h3">{loadingText}</Typography>
-                <CircularProgress />
-            </Stack>
-        }>
-            <Card sx={{
-                height: '50vmin', width: '50vmin'
-            }} id={id}>
-                <CardContent>
-                    {children}
-                </CardContent>
-            </Card >
-        </Show>
+        <Box sx={{ width: '300', height: '300' }}>
+            {showContent() ? <Grow in={showContent()} style={{ transformOrigin: "0 0 0" }}
+                {...(showContent() ? { timeout: 3000 } : {})}>
+                <Card id={id} sx={{ width: '300', height: '300' }}><CardContent><Typography variant="h4">{loadingText}</Typography></CardContent></Card></Grow>
+                : <Skeleton sx={{ bgcolor: '#212121', borderRadius: '2rem' }} variant='rectangular' height={300} width={300}></Skeleton>
+            }
+        </Box>
     )
 }
 
@@ -30,21 +22,32 @@ export default function DemoRRPath() {
     const [gatherReady, setGatherReady] = createSignal(false)
     const [assessmentReady, setAssessmentReady] = createSignal(false)
 
+    onMount(() => {
+        setTimeout(() => setSetupReady(true), 10000)
+    })
+
+    createMemo((prev) => {
+        if (setupReady() && prev != setupReady()) {
+            setTimeout(() => setGatherReady(true), 20000)
+        }
+    }, false)
+
     return (
         <Stack
             direction='row'
             justifyContent="space-around"
             alignItems="center"
-            divider={<ArrowRightAlt sx={{
-                bgcolor: 'white'
-            }} />}
+        // divider={<ArrowRightAlt sx={{
+        //     bgcolor: 'white'
+        // }} />}
         >
-            {/* <PathItem header="Setting up rapid response..." id='setup-card' showContent={setupReady()} />
-            <PathItem header="Searching YouTube and gathering videos..." id='gather-card' showContent={gatherReady()} />
-            <PathItem header="Assessment" id='assess-card' showContent={assessmentReady()} /> */}
-            <PathItem loadingText='Setting up...' showContent={setupReady()} id='setup-card' />
-            <Avatar src={logo} variant="square" sx={{ height: '300px', width: '300px' }} />
-            <Avatar src={logo} variant="square" sx={{ height: '300px', width: '300px' }} />
+            <PathItem loadingText="Setting up" showContent={setupReady} id='setup-step' />
+            <Show when={setupReady()}>
+                <PathItem loadingText="Collecting YouTube data" showContent={gatherReady} id='gather-step' />
+            </Show>
+            <Show when={gatherReady()}>
+                <PathItem loadingText="Starting assessments" showContent={assessmentReady} id='assess-step' />
+            </Show>
         </Stack >
     )
 }
